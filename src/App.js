943a4617e11b9f6be28ade8992f253af4e6a7c8b
Praveen Ramachandran <praveen.thayikkattil@gmail.com>
2020-08-10 09:00:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Mosh Hamedani</strong>
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tutorial App <code>01 React</code>
         </a>
       </header>
     </div>
