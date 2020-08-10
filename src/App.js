0c53d3839a5c7a23c63962108a552fb2f2342f7a
Praveen Ramachandran <praveen.thayikkattil@gmail.com>
2020-08-10 09:09:39 +0530

    Added git repo link

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/praveen-thayikkattil/mosh01-react-app"
           target="_blank"
           rel="noopener noreferrer"
         >
